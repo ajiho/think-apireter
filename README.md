919376c5b30916105ade920c53e7220aa8fc18c1
欲饮琵琶码上催 <lujiahao@88.com>
2023-10-02 23:14:17 +0800

    优化响应方法

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 - 支持多语言
 - 支持自定义字段
 - 支持直接抛异常,方便喜欢AOP思想处理响应的开发人员
+- 支持在任意地方调用(中间件、异常类中等)
 
 # 安装
 
@@ -14,5 +15,100 @@
 composer require ajiho/think-response
 ```
 
+# 配置文件
+
+安装完成后会在`config`目录下生成两个配置文件`response.php`、`status.php`
+
+`response.php`
+
+```php
+<?php
+
+/**
+ * 该文件主要用于配置think-response包的一些设置
+ * 假如你想让你输出字段变成 status message result  可以在下面进行更改
+ */
+
+return [
+    // 响应code变量
+    'code_var' => 'code',
+    // 响应msg变量
+    'msg_var' => 'msg',
+    //响应data变量
+    'data_var' => 'data',
+];
+
+```
+
+`status.php`
+
+```php
+<?php
+
+/**
+ * 该文件存放业务状态码相关的配置,可以用来进行多语言适配
+ */
+
+return [
+    200 => '请求成功',
+    500 => '请求错误',
+
+    // 定义其它的业务状态码
+    // 20000 => "用户名已经被注册",
+    // 20001 => "验证码不正确",
+];
+
+```
+
 # 使用
 
+```php
+use ajiho\response\Response;
+
+public function index()
+{
+    $users = User::all();
+
+    return Response::ok($users);
+}
+```
+
+## 多语言
+
+PS:如果您没有一点多语言基础可以先看官方文档多语言的基本使用
+
+需要开启多语言的中间件(如果是多应用需要两个地方都开启,否则无效)
+
+比如此时,你想给状态码配置文件`status.php`新增一个英文的语言包
+
+需要新建`status.php`
+
+```
+// 单应用模式
+app/lang/en-us/status.php
+
+// 多应用模式
+app/应用/lang/en-us/status.php
+```
+
+```php
+<?php
+
+/**
+ * 该文件存放业务状态码相关的配置,可以用来进行多语言适配
+ */
+
+return [
+    200 => 'success',
+    500 => 'error',
+
+    // 定义其它的业务状态码
+    // 20000 => "用户名已经被注册",
+    // 20001 => "验证码不正确",
+];
+
+```
+
+## 抛异常
+
+todo
--- src/Response.php
+++ src/Response.php
@@ -3,28 +3,75 @@
 namespace ajiho\response;
 
 use think\facade\Config;
+use think\facade\Lang;
+
 
 class Response
 {
 
-    protected function ok($data = [], $code = 200, $msg = '', $httpStatus = 200)
+    /**
+     * 通用的响应
+     * @param int $code 业务状态码
+     * @param string $msg 响应消息
+     * @param array $data 返回数据
+     * @param int $httpStatus http状态码
+     * @return \think\Response
+     */
+    public static function response($code = 200, $msg = '', $data = [], $httpStatus = 200)
     {
         $res = [
             Config::get('response.code_var') => $code,
-            Config::get('response.msg_var') => $msg,
+            Config::get('response.msg_var') => empty($msg) ? static::getMsg($code) : $msg,
             Config::get('response.data_var') => $data
         ];
-        return json($res, $httpStatus);
+        $res = json_encode($res, JSON_UNESCAPED_SLASHES + JSON_UNESCAPED_UNICODE);
+        $header = [
+            'Content-Type' => 'application/json; charset=utf-8'
+        ];
+        return \think\Response::create($res)->header($header)->code($httpStatus);
     }
 
 
-    protected function fail($code = 500, $msg = '', $data = [], $httpStatus = 200)
+    /**
+     * 成功的响应
+     * @param $data array 返回的数据
+     * @param $code int 业务状态码
+     * @param $msg string 返回消息
+     * @param $httpStatus int http状态码
+     * @return \think\Response
+     */
+    public static function success($data = [], $code = 200, $msg = '', $httpStatus = 200)
     {
-        $res = [
-            Config::get('response.code_var') => $code,
-            Config::get('response.msg_var') => $msg,
-            Config::get('response.data_var') => $data
-        ];
-        return json($res, $httpStatus);
+        return static::response($code, $msg, $data, $httpStatus);
+    }
+
+
+    /**
+     * 失败的响应
+     * @param $code int 业务状态码
+     * @param $msg string 返回消息
+     * @param $data array 返回的数据
+     * @param $httpStatus int http状态码
+     * @return \think\Response
+     */
+    public static function fail($code = 500, $msg = '', $data = [], $httpStatus = 200)
+    {
+        return static::response($code, $msg, $data, $httpStatus);
+    }
+
+
+
+    private static function getMsg($code)
+    {
+        $range = Lang::getLangSet();
+        $filePath = app()->getAppPath() . 'lang/' . $range . '/status.php';
+        if (file_exists($filePath)) {
+            Lang::load($filePath, $range);
+            //判断是否存在该多语言配置
+            if (Lang::has($code)) {
+                return Lang::get($code);
+            }
+        }
+        return array_key_exists($code, Config::get('status')) ? Config::get('status')[$code] : '';
     }
 }
